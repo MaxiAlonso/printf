ddb3cb68e3116af405c73c7d5967adf4df9d9d9c
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-12 13:44:36 +0000

    First functions test

---
+++ printf_functions.c
@@ -0,0 +1,38 @@
+#include "printf.h"
+/**
+ * print_number - Print a integer.
+ * @args: Arguments that passed.
+ * Return: Return 0.
+ */
+int print_number(va_list args)
+{
+	int n;
+	unsigned int num;
+	unsigned int positive;
+	unsigned int count;
+
+	va_arg(args, int) = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = (n * (-1));
+	}
+	else
+	{
+		num = n;
+	}
+	positive = num;
+	count = 1;
+
+	while (positive > 9)
+	{
+		positive /= 10;
+		count *= 10;
+	}
+	for ( ; count >= 1; count /= 10)
+	{
+		_putchar(((num / count) % 10) + '0');
+	}
+	return (0);
+}
