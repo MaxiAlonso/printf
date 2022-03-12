1e9154b8ee257a8dade5e1dcf71185b56144fed0
MaxiHBTN <4387@holbertonschool.com>
2022-03-12 18:33:17 +0000

    Header

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct pri - struct
+ * @pr: a
+ * @f: b
+ **/
+
+typedef struct pri
+{
+	char *pr;
+	int (*f)(va_list args);
+} print;
+
+int _printf(const char *format, ...);
+int print_number(va_list args);
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percentage();
+#endif
