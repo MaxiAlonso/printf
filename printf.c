721cbf32d99ac3d3b280d195b84043ea35c74e9c
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-12 17:56:50 +0000

    Printf

--- printf.c
+++ printf.c
@@ -1,5 +1,4 @@
 #include "printf.h"
-
 /**
  * _printf - produce output according to a format.
  * @format: - define the format to print.
@@ -11,25 +10,31 @@ int _printf(const char *format, ...)
 	print myprint[] = {
 		{"d", print_number},
 		{"i", print_number},
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percentage},
 		{NULL, NULL},
 	};
 
 	int count, i;
 	va_list args;
+	int rcount;
 
 	va_start(args, format);
 	i = 0;
+	rcount = 0;
 
 	while (format != NULL && format[i] != '\0')
 	{
 		count = 0;
 		if (format[i] == '%')
 		{
-			while (count < 2)
+			while (count < 5)
 			{
 				if (format[i + 1] == myprint[count].pr[0])
 				{
-					myprint[count].f(args);
+					rcount	+= (myprint[count].f(args)) - 2;
+					break;
 				}
 				count++;
 			}
@@ -42,5 +47,5 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(args);
-	return (i);
+	return (i + rcount);
 }
