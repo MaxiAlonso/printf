5cf55fe3f4c4a622b54a5936bdc052df7c67d5f8
MaxiHBTN <4387@holbertonschool.com>
2022-03-13 11:47:15 +0000

    Print advanced options

--- printf_functions_one.c
+++ printf_functions_one.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_binary - convert a number into binary and print it
@@ -8,11 +9,11 @@
 
 int print_binary(va_list args)
 {
-	int num, a, i, rcount;
+	int num, a, rcount, i = 0;
 	char *str;
 
 	num = va_arg(args, int);
-	a = va_arg(args, int);
+	a = num;
 
 	while (a > 0)
 	{
@@ -73,7 +74,7 @@ int print_unsigned(va_list args)
 
 /**
  * print_octal - Print number in base 8.
- * @arg: Argument passed.
+ * @args: Argument passed.
  * Return: Return lenght of number.
  */
 int print_octal(va_list args)
@@ -82,7 +83,7 @@ int print_octal(va_list args)
 	char *str;
 
 	num = va_arg(args, int);
-	a = va_arg(args, int);
+	a = num;
 
 	while (a > 0)
 	{
@@ -105,6 +106,105 @@ int print_octal(va_list args)
 		i--;
 	}
 	_putchar(str[i]);
+	free(str);
+	return (rcount);
+}
+
+/**
+ * print_hexalow - Print number in base 16 lowercase.
+ * @args: Argument passed.
+ * Return: Return lenght of number.
+ */
+int print_hexalow(va_list args)
+{
+	unsigned int num, a, rcount, i;
+	char *str;
+
+	num = va_arg(args, int), a = num;
+	while (a > 0)
+		a /= 16, i++;
+	rcount = i, str = malloc((sizeof(char) * i) + 1), i = 0;
+	while (num > 0)
+	{
+		if ((num % 16) <= 9)
+			str[i] = (num % 16) + '0';
+		else
+			switch (num % 16)
+			{
+				case 10:
+					str[i] = 'a';
+					break;
+				case 11:
+					str[i] = 'b';
+					break;
+				case 12:
+					str[i] = 'c';
+					break;
+				case 13:
+					str[i] = 'd';
+					break;
+				case 14:
+					str[i] = 'e';
+					break;
+				case 15:
+					str[i] = 'f';
+					break;
+			}
+		num /= 16, i++;
+	}
+	while (i != 0)
+		_putchar(str[i]), i--;
+	_putchar(str[i]);
+	free(str);
+	return (rcount);
+}
+
+/**
+ * print_hexaupp - Print number in base 16 uppercase.
+ * @args: Argument passed.
+ * Return: Return lenght of number.
+ */
+int print_hexaupp(va_list args)
+{
+	unsigned int num, a, rcount, i;
+	char *str;
+
+	num = va_arg(args, int), a = num;
+	while (a > 0)
+		a /= 16, i++;
+	rcount = i, str = malloc((sizeof(char) * i) + 1), i = 0;
+	while (num > 0)
+	{
+		if ((num % 16) <= 9)
+			str[i] = (num % 16) + '0';
+		else
+			switch (num % 16)
+			{
+				case 10:
+					str[i] = 'A';
+					break;
+				case 11:
+					str[i] = 'B';
+					break;
+				case 12:
+					str[i] = 'C';
+					break;
+				case 13:
+					str[i] = 'D';
+					break;
+				case 14:
+					str[i] = 'E';
+					break;
+				case 15:
+					str[i] = 'F';
+					break;
+			}
+		num /= 16, i++;
+	}
+	while (i != 0)
+		_putchar(str[i]), i--;
+	_putchar(str[i]);
+	free(str);
 	return (rcount);
 }
 
