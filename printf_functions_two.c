59f80975e845fd1a07e1dd02ac401775caff0281
MaxiHBTN <4387@holbertonschool.com>
2022-03-13 12:39:54 +0000

    Print advanced options two

--- printf_functions_two.c
+++ printf_functions_two.c
@@ -22,3 +22,38 @@ int print_rev(va_list args)
 	}
 	return (i);
 }
+
+/**
+ * print_rot13 - Print string in rot13
+ * @args: Argument passed
+ * Return: Return lenght of string
+ */
+
+int print_rot13(va_list args)
+{
+	char *str;
+	int i = 0;
+
+	str = va_arg(args, char *);
+
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+			{
+				_putchar(str[i] + 13);
+			}
+			else
+			{
+				_putchar(str[i] - 13);
+			}
+		}
+		else
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	return (i);
+}
